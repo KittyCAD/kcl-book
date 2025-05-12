083f32074bfa2317c0cd62bb3d624ac7fd458b26
Adam Sunderland <iterion@gmail.com>
2025-05-12 15:40:09 -0400

    Use correct deploy dir

--- vercel-build.sh
+++ vercel-build.sh
@@ -2,4 +2,4 @@ curl -Lo mdbook-toc.tar.gz https://github.com/badboy/mdbook-toc/releases/downloa
 curl -Lo mdbook.tar.gz https://github.com/rust-lang/mdBook/releases/download/v0.4.49/mdbook-v0.4.49-x86_64-unknown-linux-musl.tar.gz
 tar -xvzf mdbook.tar.gz
 tar -xvzf mdbook-toc.tar.gz
-./mdbook build
+./mdbook build kcl-book
