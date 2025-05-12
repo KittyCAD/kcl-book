9d247584026f085f9c592768c6c72b883a7937ee
Adam Sunderland <iterion@gmail.com>
2025-05-12 15:43:12 -0400

    Add mdbook-toc to build script path

--- vercel-build.sh
+++ vercel-build.sh
@@ -2,4 +2,5 @@ curl -Lo mdbook-toc.tar.gz https://github.com/badboy/mdbook-toc/releases/downloa
 curl -Lo mdbook.tar.gz https://github.com/rust-lang/mdBook/releases/download/v0.4.49/mdbook-v0.4.49-x86_64-unknown-linux-musl.tar.gz
 tar -xvzf mdbook.tar.gz
 tar -xvzf mdbook-toc.tar.gz
+export PATH="$(pwd)/mdbook-toc;$PATH"
 ./mdbook build kcl-book
