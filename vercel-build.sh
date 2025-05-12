62d70fb2d978938a9944338e430f6d7abf0a44f8
Adam Sunderland <iterion@gmail.com>
2025-05-12 15:38:39 -0400

    Add vercel-build.sh

---
+++ vercel-build.sh
@@ -0,0 +1,5 @@
+curl -Lo mdbook-toc.tar.gz https://github.com/badboy/mdbook-toc/releases/download/0.14.2/mdbook-toc-0.14.2-x86_64-unknown-linux-musl.tar.gz
+curl -Lo mdbook.tar.gz https://github.com/rust-lang/mdBook/releases/download/v0.4.49/mdbook-v0.4.49-x86_64-unknown-linux-musl.tar.gz
+tar -xvzf mdbook.tar.gz
+tar -xvzf mdbook-toc.tar.gz
+./mdbook build
