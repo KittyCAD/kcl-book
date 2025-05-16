216c902d027e6585abf88ffbfbef2775507b31a0
Adam Chalmers <adam.chalmers@zoo.dev>
2025-05-16 08:44:14 -0500

    Debugging CI

--- vercel-build.sh
+++ vercel-build.sh
@@ -14,10 +14,12 @@ cd mdbook-kcl
 cargo build
 cd -
 mv mdbook-kcl/target/debug/mdbook-kcl bin
+ls bin
 
 curl -Lo mdbook-toc.tar.gz https://github.com/badboy/mdbook-toc/releases/download/0.14.2/mdbook-toc-0.14.2-x86_64-unknown-linux-musl.tar.gz
 curl -Lo mdbook.tar.gz https://github.com/rust-lang/mdBook/releases/download/v0.4.49/mdbook-v0.4.49-x86_64-unknown-linux-musl.tar.gz
 tar -xvzf mdbook.tar.gz -C bin
 tar -xvzf mdbook-toc.tar.gz -C bin
 export PATH="$(pwd)/bin:$PATH"
+which mdbook-kcl
 mdbook build kcl-book
