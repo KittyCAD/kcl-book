ca95eab978a4766e134459c34b51558f8ddc84cc
Adam Chalmers <adam.chalmers@zoo.dev>
2025-05-16 08:38:26 -0500

    Build mdbook-kcl in vercel

--- vercel-build.sh
+++ vercel-build.sh
@@ -2,8 +2,18 @@
 
 # This is a build script intended to be consumed by Vercel, though it should work locally if your platform matches.
 
+# First install Rust because we need to compile mdbook-kcl
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup install stable
+
 # Make a temporary directory for binaries
 mkdir -p bin
+
+# Compile mdbook-kcl
+cd mdbook-kcl
+cargo build
+cd -
+mv mdbook-kcl/target/debug/mdbook-kcl bin
 
 curl -Lo mdbook-toc.tar.gz https://github.com/badboy/mdbook-toc/releases/download/0.14.2/mdbook-toc-0.14.2-x86_64-unknown-linux-musl.tar.gz
 curl -Lo mdbook.tar.gz https://github.com/rust-lang/mdBook/releases/download/v0.4.49/mdbook-v0.4.49-x86_64-unknown-linux-musl.tar.gz
